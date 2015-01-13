e2249a392f51ed23f193cebecc4fa8a9d3cd87d2
michael <mderazon@hotmail.com>
2015-01-13 21:24:40 +0200

    move video to the top

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.post('/send', function(req, res, next) {
         subject: "New subscriber!",
         text: "שם: " + body.name + "\n" +
               "אימייל: " + body.email + "\n" +
+              "כתובת: " + body.address + "\n" +
               "הערה: " + body.comment,
     };
 
